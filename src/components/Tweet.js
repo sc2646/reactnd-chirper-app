d261df344d58d263a696c9fcf992f3aef27a272e
Natalie Chen <natalie.chen@Natalie-Chens-MacBook-Pro.local>
2020-03-23 15:06:57 -0700

    toggle tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti/index'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
   handleLike = (event) => {
     event.preventDefault()
     // todo: handle Like Tweet
+    const { dispatch, tweet, authedUser } = this.props
+
+    dispatch(handleToggleTweet({
+      id: tweet.id,
+      hasLiked: tweet.hasLiked,
+      authedUser
+    }))
   }
   toParent = (event, id) => {
     event.preventDefault()
