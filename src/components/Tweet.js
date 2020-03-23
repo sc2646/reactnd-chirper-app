e02210fcfb02a57072ff8badf96c0e0f9b26b1c2
Natalie Chen <natalie.chen@Natalie-Chens-MacBook-Pro.local>
2020-03-23 14:53:13 -0700

    ui for timeline

---
+++ src/components/Tweet.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { formatTweet, formatDate } from '../utils/helpers'
+import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti/index'
+
+class Tweet extends Component {
+  handleLike = (event) => {
+    event.preventDefault()
+    // todo: handle Like Tweet
+  }
+  toParent = (event, id) => {
+    event.preventDefault()
+    // todo: Redirect to parent Tweet.
+  }
+  render() {
+    const { tweet } = this.props
+
+    if (tweet === null) {
+      return <p>This Tweet doesn't exists</p>
+    }
+
+    const {
+      name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
+    } = tweet
+
+    return(
+      <div className='tweet'>
+        <img
+          src={avatar}
+          alt={`Avatar of ${name}`}
+          className='avatar'
+        />
+        <div className='tweet-info'>
+          <div>
+            <span>{name}</span>
+            <div>{formatDate(timestamp)}</div>
+            {parent && (
+              <button className='replyingTo' onClick={(event) => {this.toParent(event, parent.id)}}>
+                Replying to @{parent.author}
+              </button>
+            )}
+            <p>{text}</p>
+          </div>
+          <div className='tweet-icons'>
+            <TiArrowBackOutline className='tweet-icon' />
+            <span>{replies !== 0 && replies}</span>
+            <button className='heart-button' onClick={this.handleLike}>
+              {hasLiked === true
+                ? <TiHeartFullOutline color='#e0245e' className='tweet-icon' />
+                : <TiHeartOutline className='tweet-icoon' />}
+            </button>
+            <span>{likes !== 0 && likes}</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps ({authedUser, users, tweets}, { id }) {
+  const tweet = tweets[id]
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null
+
+  return {
+    authedUser,
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null
+  }
+}
+
+export default connect(mapStateToProps)(Tweet)
